397d265cfb4b3fa8c209a601b53256053c9754e8
lsvx <lserven@gmail.com>
2015-09-13 06:57:18 -0400

    Add route param.

--- index.js
+++ index.js
@@ -7,36 +7,37 @@ var access_token = process.env.LED_ACCESS_TOKEN;
 var app = express();
 
 function post_data(i) {
-	return querystring.stringify({
-		access_token: access_token,
-		comand: i
-	});
+    return querystring.stringify({
+        access_token: access_token,
+        command: i
+    });
 }
 
 function post_options(i) {
-	return {
-		hostname: "api.particle.io",
-		port: 443,
-		method: 'POST',
-		path: "/v1/devices/53ff70066667574811370967/led",
-		headers: {
-         		'Content-Type': 'application/x-www-form-urlencoded',
-          	'Content-Length': post_data(i).length
-      	}
-	};
+    return {
+        hostname: "api.particle.io",
+            port: 443,
+            method: 'POST',
+            path: "/v1/devices/53ff70066667574811370967/led",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': post_data(i).length
+            }
+    };
 }
-		
 
-app.get('/', function(req, res) {
-	var request = https.request(post_options(100), function(res2) {
-		res2.setEncoding('utf8');
-      	res2.on('data', function (chunk) {
-          	console.log('Response: ' + chunk);
-     	});
-		res.end();
-	});
-	request.write(post_data(100));
-	request.end();
+
+app.get('/:id?', function(req, res) {
+    var id = !isNaN(req.params.id) ? req.params.id : 100;
+    var request = https.request(post_options(id), function(res2) {
+        res2.setEncoding('utf8');
+        res2.on('data', function (chunk) {
+            console.log('Response: ' + chunk);
+        });
+        res.end();
+    });
+    request.write(post_data(id));
+    request.end();
 });
 
 app.listen(8080);
